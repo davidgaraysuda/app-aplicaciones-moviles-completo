c6c59d1ad6570cd52d8c5972425651cfc4e715a8
Danymat28 <117756174+davidpozoio@users.noreply.github.com>
2022-12-15 11:36:30 -0500

    prueba app moviles

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { useState, useRef } from 'react';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+
+import Button from './components/Button';
+import ArgentinaList from './components/ArgentinaList';
+import FranciaList from './components/FranciaList';
+import MarruecosList from './components/MarruecosList';
+
 
 export default function App() {
+  const [showArgentinaOptions, setShowArgentinaOptions] = useState(false);
+  const [showFranciaOptions, setShowFranciaOptions] = useState(false);
+  const [showMarruecosOptions, setShowMarruecosOptions] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const onReset = () => {
+    setShowFranciaOptions(false);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
+     <View style={styles.container}>
+      <Button theme="primary" label="Argentina" onPress={() => setShowArgentinaOptions(true)}/>
+      {showArgentinaOptions ? (
+        <ArgentinaList/>)
+      : (
+      <View style={styles.footerContainer}>
+      </View>
+      )}
+      
+      <Button theme="primary" label="Francia" onPress={() => setShowFranciaOptions(true)}/>
+      {showFranciaOptions ? (
+        <FranciaList/>)
+      : (
+        <View style={styles.footerContainer}>
+      </View>
+      )}
+      
+      <Button theme="primary" label="Marruecos" onPress={() => setShowMarruecosOptions(true)}/>
+      {showMarruecosOptions ? (
+        <MarruecosList/>)
+      : (
+      <View style={styles.footerContainer}>
+      </View>
+      )}
+      
     </View>
   );
 }
